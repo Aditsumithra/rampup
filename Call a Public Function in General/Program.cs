a926a50138e39156baf333a45d6c801e49ce9c4c
Aditsv27 <sumithravenkataramanan@gmail.com>
2025-02-17 18:01:40 +0530

    Initial commit - adding project files

---
+++ Call a Public Function in General/Program.cs
@@ -0,0 +1,16 @@
+﻿class MyClass
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyClass obj = new MyClass();  // Creating an instance
+        obj.SayHello();  // Calling the public function
+    }
+}
